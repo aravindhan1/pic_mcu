b57528fabfc91550d0b9cb364b3ed76329bdc6c8
aravindhan1 <aravindh5445@gmail.com>
2026-01-29 12:12:25 +0530

    7 seg mux

--- 7_seg/7_seg.X/main.c
+++ 7_seg/7_seg.X/main.c
@@ -2,22 +2,31 @@
  * File:   main.c
  * Author: aravindhan-tv
  *
- * Created on January 29, 2026, 10:27 AM
+ * Created on January 28, 2026, 6:03 PM
  */
 
 //PORTb PORTD TRISB are predefines as addresses
 #include <xc.h>
+#define _XTAL_FREQ 20000000
 
-//turn off WDT (Watch dog timer )
-#pragma config WDTE=OFF
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
-void init_config() {
-
-}
-
+unsigned char segment[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7c, 0x07, 0x7f, 0x6f}, i = 0;
 void main(void) {
-    init_config();
+    TRISD = 0x00;
     while (1) {
+        for (i=0;i<10;i++){
+            PORTD = segment[i];
+            __delay_ms(1000);
+        }
 
     }
 }
---
+++ 7_seg/mux_7seg.X/main.c
@@ -0,0 +1,40 @@
+/*
+ * File:   main.c
+ * Author: aravindhan-tv
+ *
+ * Created on January 29, 2026, 11:06 AM
+ */
+
+//PORTb PORTD TRISB are predefines as addresses
+#include <xc.h>
+
+//turn off WDT (Watch dog timer )
+#pragma config WDTE=OFF
+#define _XTAL_FREQ 20000000
+#define segment_on 1
+#define segment_off 0
+
+unsigned char  number = 78;
+unsigned char i,j; 
+unsigned char seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+
+void main(void) {
+    TRISB = 0x00;
+    TRISC = 0x00;
+    while (1) {
+        i = number/10;
+        j = number%10;
+        //1. turn on the d1 and turn off the d2
+        PORTCbits.RC0 = segment_on;
+        PORTCbits.RC1 = segment_off;
+        //2. Print req data in d1
+        PORTB = seg[i];
+        __delay_ms(10);
+        //3. turn on d2 and off d1
+        PORTCbits.RC1 = segment_on;
+        PORTCbits.RC0 = segment_off;
+        //4. Print req data on d2
+        PORTB = seg[j];
+        __delay_ms(10);    
+    }
+}
